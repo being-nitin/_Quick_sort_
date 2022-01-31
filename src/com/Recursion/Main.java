5aa45442a0b6fb6a4f536317aa69d4443c1ae1a7
Nitin <nsinghboss02@gmail.com>
2022-01-31 23:22:23 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,15 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/* Quick sort
+	   For best case the time complexity is o(nlogn)
+	   For worst case the time complexity is o(n^2).
+
+	 */
+
+
+    }
+    
+}
