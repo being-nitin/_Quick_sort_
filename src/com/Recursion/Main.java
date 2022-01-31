01966046b62decea39aec51e38f385e1fc57baa5
Nitin <nsinghboss02@gmail.com>
2022-01-31 23:25:37 +0530

    Best case condition added

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -6,10 +6,11 @@ public class Main {
 	/* Quick sort
 	   For best case the time complexity is o(nlogn)
 	   For worst case the time complexity is o(n^2).
-
+	   Best case means the pivot is the mid element so that it divides the array in two halves. And at the
+	   correct index too.
 	 */
 
 
     }
-    
+
 }
