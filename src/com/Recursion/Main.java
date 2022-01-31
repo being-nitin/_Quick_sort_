ed463af9b609c636e298e6fcc28304b7da913f4a
Nitin <nsinghboss02@gmail.com>
2022-01-31 23:29:09 +0530

    worst  case condition added

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -7,7 +7,10 @@ public class Main {
 	   For best case the time complexity is o(nlogn)
 	   For worst case the time complexity is o(n^2).
 	   Best case means the pivot is the mid element so that it divides the array in two halves. And at the
-	   correct index too.
+	   correct index too. So the time complexity will be o(nlogn).
+	   Worst case means the pivot which we have selected is the max element or min element,
+	   that means a part of the array is vacant.That is the value of k =0. so the time complexity
+	   will be o(n^2).
 	 */
 
 
